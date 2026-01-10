499c649549efc221f0f8151503549eb0a4122976
Tushar Upadhyay <152595076+DeepMonarch@users.noreply.github.com>
2026-01-10 18:58:02 +0530

    Implement voting eligibility check
    Added voting eligibility check based on age.

--- c++/BASICS/07_conditional.cpp
+++ c++/BASICS/07_conditional.cpp
@@ -1,5 +1,11 @@
 #include <iostream>
 using namespace std;
 int main(){
+    int n = 19
+    if(n>18){
+        cout<<"Eligible for voting"<<endl;
+    } else {
+        cout<<"not eligible to vote"<<endl;
     
+
 }
